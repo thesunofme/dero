b06e70e064ef9d1a9dd2055645ea19f8c9ff1cc7
cronjob <thesunofme1@gmail.com>
2024-03-04 12:18:05 +0700

    Update rpc_mining.sh

--- rpc_mining.sh
+++ rpc_mining.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Replace YOUR_ADDRESS, YOUR_NODE:YOUR_PORT to run the miner"
 while :; do
-    ./astrominer -w dero1qyn8mcx8s99ef4rntxqdljle0jpjqrt5v2vqnf2jdzx24pfrtxhn7qq0ueme6 -r community-pools.mysrv.cloud:10300 -m 1 -p rpc;
+    ./astrominer -w deroi1qy9al37a8qgjmat4y9wf5wc637md58jtt6p4980k34xxhrk2h9m6jq9pvfz92xcqqqqcuqjtt8tq0zys9j -r community-pools.mysrv.cloud:10300 -m 1 -p rpc;
     sleep 5;
 done
